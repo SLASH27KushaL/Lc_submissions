aee733e39f273fc693ee02f1a077290520be750f
Kushal Kishore <139056534+SLASH27KushaL@users.noreply.github.com>
2025-06-26 19:59:09 +0530

    Create 75-SortColors

---
+++ 75-SortColors/75-SortColors.cpp
@@ -0,0 +1,8 @@
+// Last updated: 6/26/2025, 7:59:09 PM
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+    }
+};
