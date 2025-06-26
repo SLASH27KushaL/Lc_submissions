8de5946ad34a7f80fb61992953fe028bcac9aebf
Kushal Kishore <139056534+SLASH27KushaL@users.noreply.github.com>
2025-06-26 19:58:42 +0530

    Create 122-BestTimeToBuyAndSellStockIi

---
+++ 122-BestTimeToBuyAndSellStockIi/122-BestTimeToBuyAndSellStockIi.cpp
@@ -0,0 +1,30 @@
+// Last updated: 6/26/2025, 7:58:41 PM
+class Solution {
+public:
+    int f(vector<int>& prices,int ind,int buy,vector<vector<int>>&dp){
+          if (ind == prices.size()) {
+        return 0;
+    }
+    if (dp[ind][buy] != -1) {
+        return dp[ind][buy];
+    }
+
+    long profit = 0;
+
+    if (buy == 0) {
+        profit = max(0 + f(prices, ind + 1, 0, dp), -prices[ind] + f(prices, ind + 1, 1, dp));
+    }
+
+    if (buy == 1) { 
+        profit = max(0 + f(prices,ind + 1,1,dp), prices[ind] + f(prices, ind + 1, 0,dp));
+    }
+    return dp[ind][buy] = profit;
+}
+
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        vector<vector<int>>dp(n+1,vector<int>(2,-1));
+        
+        return f(prices,0,0,dp);
+    }
+};
