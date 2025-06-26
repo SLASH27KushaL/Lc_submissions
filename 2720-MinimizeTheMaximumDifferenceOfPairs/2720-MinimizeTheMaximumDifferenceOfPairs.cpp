75b3043f996f96937ac9c2ffa23119f7b9f8af46
Kushal Kishore <139056534+SLASH27KushaL@users.noreply.github.com>
2025-06-26 19:54:03 +0530

    Create 2720-MinimizeTheMaximumDifferenceOfPairs

---
+++ 2720-MinimizeTheMaximumDifferenceOfPairs/2720-MinimizeTheMaximumDifferenceOfPairs.cpp
@@ -0,0 +1,33 @@
+// Last updated: 6/26/2025, 7:54:02 PM
+class Solution {
+public:
+    int countValidPairs(vector<int>& nums, int threshold) {
+        int index = 0, count = 0;
+        while (index < nums.size() - 1) {
+            
+            if (nums[index + 1] - nums[index] <= threshold) {
+                count++;
+                index++;
+            }
+            index++;
+        }
+        return count;
+    }
+
+    int minimizeMax(vector<int>& nums, int p) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        int left = 0, right = nums[n - 1] - nums[0];
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (countValidPairs(nums, mid) >= p) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+};
