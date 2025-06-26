46107d56f667493a67d2e5fbd3a223b427faa716
Kushal Kishore <139056534+SLASH27KushaL@users.noreply.github.com>
2025-06-26 19:53:51 +0530

    Create 3194-FindWordsContainingCharacter

---
+++ 3194-FindWordsContainingCharacter/3194-FindWordsContainingCharacter.cpp
@@ -0,0 +1,13 @@
+// Last updated: 6/26/2025, 7:53:51 PM
+class Solution {
+public:
+    vector<int> findWordsContaining(vector<string>& words, char x) {
+        vector<int> ans;
+        for (int i = 0; i < words.size(); i++) {
+            if (words[i].find(x) != string::npos) {
+                ans.push_back(i);
+            }
+        }
+        return ans;
+    }
+};
