4f5e4f0bf096149c7e3e1288b90e8b95a1c92ef2
Kushal Kishore <139056534+SLASH27KushaL@users.noreply.github.com>
2025-06-26 19:52:41 +0530

    Create 3859-MaximumProductOfTwoDigits

---
+++ 3859-MaximumProductOfTwoDigits/3859-MaximumProductOfTwoDigits.cpp
@@ -0,0 +1,13 @@
+// Last updated: 6/26/2025, 7:52:41 PM
+class Solution {
+public:
+    int maxProduct(int n) {
+        vector <int> digs;
+        while (n > 0) {
+            digs.push_back(n%10);
+            n /= 10;
+        }
+        sort(digs.begin(), digs.end());
+        return digs[digs.size()-1] * digs[digs.size()-2];
+    }
+};
