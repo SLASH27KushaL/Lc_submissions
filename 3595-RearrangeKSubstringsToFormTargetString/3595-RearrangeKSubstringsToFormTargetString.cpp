7f99a08d7bab4246e127e9c05eb43397074bcd9d
Kushal Kishore <139056534+SLASH27KushaL@users.noreply.github.com>
2025-07-15 04:17:01 +0530

    Create 3595-RearrangeKSubstringsToFormTargetString

---
+++ 3595-RearrangeKSubstringsToFormTargetString/3595-RearrangeKSubstringsToFormTargetString.cpp
@@ -0,0 +1,15 @@
+// Last updated: 7/15/2025, 4:17:00 AM
+class Solution {
+public:
+    bool isPossibleToRearrange(string s, string t, int k) {
+        int n = s.size();
+        unordered_map<string, int> mp;
+        int size = n / k;
+        for (int i = 0; i < n; i += size) mp[s.substr(i, size)]++;
+        for (int i = 0; i < n; i += size) mp[t.substr(i, size)]--;
+        
+        for (auto &[key,value] : mp) if (value != 0) return false;
+        
+        return true;
+    }
+};
